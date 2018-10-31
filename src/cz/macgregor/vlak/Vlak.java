62e4fdc0831c41359a3ee74f7c98ef67adfec210
MacGregor <Filda.Gregor@seznam.cz>
2018-10-31 21:14:30 +0100

    !modified size to 1366x768 gitignore

--- src/cz/macgregor/vlak/Vlak.java
+++ src/cz/macgregor/vlak/Vlak.java
@@ -12,31 +12,31 @@ import cz.macgregor.vlak.grf.MapLabel;
 
 public class Vlak {
 
-  public static void main(String[] args) {
-    int width = 40;
-    int height = 22;
-    int fieldWidth = 40;
-    int fieldHeight = 40;
+	public static void main(String[] args) {
+		int width = 32;
+		int height = 16;
+		int fieldWidth = 40;
+		int fieldHeight = 40;
 
-    GameMap map = new GameMap(width, height);
-    map.addObject(10, 10, Field.GROUND, new GameObject(Sprite.LOCO));
-    map.addObject(11, 10, Field.GROUND, new GameObject(Sprite.VAGON1));
-    map.addObject(12, 10, Field.GROUND, new GameObject(Sprite.VAGON1));
-    map.addObject(13, 10, Field.GROUND, new GameObject(Sprite.VAGON1));
+		GameMap map = new GameMap(width, height);
+		map.addObject(10, 10, Field.GROUND, new GameObject(Sprite.LOCO));
+		map.addObject(11, 10, Field.GROUND, new GameObject(Sprite.VAGON1));
+		map.addObject(12, 10, Field.GROUND, new GameObject(Sprite.VAGON1));
+		map.addObject(13, 10, Field.GROUND, new GameObject(Sprite.VAGON1));
 
-    JFrame frame = new JFrame();
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setTitle("Vlak");
+		JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Vlak");
 
-    MapLabel label = new MapLabel(map, fieldWidth, fieldHeight);
-    frame.add(label, BorderLayout.CENTER);
+		MapLabel label = new MapLabel(map, fieldWidth, fieldHeight);
+		frame.add(label, BorderLayout.CENTER);
 
-    frame.pack();
-    Utils.moveFrameToCenter(frame);
-    frame.setVisible(true);
+		frame.pack();
+		Utils.moveFrameToCenter(frame);
+		frame.setVisible(true);
 
-    label.repaint();
+		label.repaint();
 
-  }
+	}
 
 }
